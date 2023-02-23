f100eb46f45793a9d15d52c52465fba9f270dce4
Longtao Zhang <DragonBillow@outlook.com>
2023-02-23 13:42:32 +0800

    Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::net::TcpListener;
 
+use std::io::Write;
+
 fn main() {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accpted new connection");
+            Ok(mut stream) => {
+                let response = "+PONG\r\n".as_bytes();
+                stream.write_all(&response).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
