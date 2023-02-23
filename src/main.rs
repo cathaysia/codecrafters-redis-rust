a2980c65e75d24eb11e02ec69cb3b8dafc4dc9c7
Longtao Zhang <DragonBillow@outlook.com>
2023-02-23 13:55:02 +0800

    Respond to multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use std::io::Write;
+use std::io::{BufRead, BufReader, Read, Write};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -9,8 +9,18 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
+                let mut buf = [0; 1024];
                 let response = "+PONG\r\n".as_bytes();
-                stream.write_all(&response).unwrap();
+
+                let reader = BufReader::new(stream.try_clone().unwrap());
+
+                for line in reader.lines() {
+                    let line = line.unwrap();
+                    dbg!(&line);
+                    if line.trim() == "ping" {
+                        stream.write(response).unwrap();
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
